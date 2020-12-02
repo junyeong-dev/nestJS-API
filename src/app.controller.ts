23914852a89fa12563043ea3a31842997710fd92
Jun-cloud <kjy8827@gmail.com>
2020-12-02 22:04:49 +0900

    Controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,13 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  // Controller은 url로의 요청을 받음
+  // '/nestjs' 가 url이 됨
+  // '/nestjs'로 들어오면 getNestjs()함수가 실행됨
+  @Get('/nestjs')
+  getNestjs(): string {
+    return 'nestjs';
+  }
+  
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 // @~ : nestjs에서는 데코레이션 이라고 명명
+// AppModule은 모든 모듈의 루트같은 것
 @Module({
   imports: [],
   controllers: [AppController],
