de5ff77578434e8bb85193e05411215f00c81b23
Jun-cloud <kjy8827@gmail.com>
2020-12-03 21:02:50 +0900

    Service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,12 +10,17 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  // Controller은 url로의 요청을 받음
-  // '/nestjs' 가 url이 됨
-  // '/nestjs'로 들어오면 getNestjs()함수가 실행됨
+  /* 
+    Controller은 url로의 요청을 받음
+    '/nestjs' 가 url이 됨
+    '/nestjs'로 들어오면 getNestjs()함수가 실행됨
+    Controller에서는 기본적으로 url을 가져오거나 함수를 리턴(실행)하는 역할을 함 
+    비즈니스 로직은 Service에서 처리
+    Controller의 함수명과 Service의 함수명이 꼭 같은 필요는 없음
+  */
   @Get('/nestjs')
   getNestjs(): string {
-    return 'nestjs';
+    return this.appService.getNestjs();
   }
   
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
+// Service 실질적인 비즈니스 로직을 실행하는 곳
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello Nest!';
+  }
+  getNestjs(): string {
+    return 'Nestjs';
   }
 }
