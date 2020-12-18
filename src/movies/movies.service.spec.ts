76a1e0f10c71b46e3384582ec898759f02de265f
Jun-cloud <kjy8827@gmail.com>
2020-12-18 23:48:41 +0900

    Testing getAll and getOne

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -19,6 +20,34 @@ describe('MoviesService', () => {
 
   it('should be test', () => {
     expect(2+2).toEqual(4);
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should retun a movie', () => {
+      service.create({
+        title: 'Test',
+        genres: ['T', 'E', 'S', 'T'],
+        year: 2020
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(404);
+      } catch(e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Id not found');
+      }
+    });
   });
 
 });
