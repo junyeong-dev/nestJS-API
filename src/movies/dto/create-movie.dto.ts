93d7024f0a213d9e09829ca5d601567da10553e4
Jun-cloud <kjy8827@gmail.com>
2020-12-26 09:45:54 +0900

    Add Comment

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,11 +1,16 @@
 import { IsString, IsNumber, IsOptional } from "class-validator";
 
 // DTO : Data Transfer Object 계층간 데이터 교환을 위한 클래스
+// class-validator를 통해 Data Validation(데이터 유효성 검사)을 함
+// @IsString(), @IsNumber(), @IsDate() 등등 여러가지 옵션이 있음
+// Referance : https://github.com/typestack/class-validator#validation-decorators
 export class CreateMovieDto {
 
+    // String 타입
     @IsString()
     readonly title: string;
 
+    // Number 타입
     @IsNumber()
     readonly year: number;
     
